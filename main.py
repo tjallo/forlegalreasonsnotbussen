ed9d30c15d2e38cbe40aba5d78a099cb8ad444e7
Tjalle <31376864+tjallo@users.noreply.github.com>
2020-06-04 16:25:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ import gameLogic.players as players
 
 players.addPlayer("Tjalle")
 
+print(f"playercount: {players.getPlayerCount()}")
+print(f"player add index 0: {players.getPlayer(0)}")
+
 #Get 52 cards, and parses them to human readable format en prints it to console
-for i in range(52):
-    card = cards.getCard()
-    parsed = cards.cardParser(card[0], card[1])
-    print(parsed)
+# for i in range(52):
+#     card = cards.getCard()
+#     parsed = cards.cardParser(card[0], card[1])
+#     print(parsed)
