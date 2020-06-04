366192bba815b0ada0a4658ad097ba489fa0607e
Tjalle <31376864+tjallo@users.noreply.github.com>
2020-06-04 16:14:22 +0200

    more comments

--- gameLogic/players.py
+++ gameLogic/players.py
@@ -1,10 +1,13 @@
 players = []
 
+#Add player object to array with name provided (rest of the entries are boilerplate for later code)
 def addPlayer(name):
     players.append([name, 0, []])
 
+#Returns number of players in player array
 def getPlayerCount():
     return len(players)
 
+#Return player at index
 def getPlayer(index):
     return players[index]
