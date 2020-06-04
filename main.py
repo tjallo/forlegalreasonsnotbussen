ae9a727c4dcc9abff3feb55b7e25ba3927327eae
Tjalle <31376864+tjallo@users.noreply.github.com>
2020-06-04 16:12:16 +0200

    added comments as reminders for myself

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+#import dependencies
 import random
 
+#import local libraries
 import gameLogic.cards as cards
 import gameLogic.players as players
 
 players.addPlayer("Tjalle")
 
+#Get 52 cards, and parses them to human readable format en prints it to console
 for i in range(52):
     card = cards.getCard()
     parsed = cards.cardParser(card[0], card[1])
