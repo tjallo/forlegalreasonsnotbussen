3977f564ef20d95285395ad9e6fe69aba4ceafcb
TjalleWired <65167704+TjalleWired@users.noreply.github.com>
2020-06-06 18:50:49 +0200

    added working logic for third round of round 1

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def startGame():
     print("The following players are playing:")
     for i in range(players.getPlayerCount()):
         print(players.getPlayer(i)[0])
-
+#First rouund of round 1, red or black
 def redBlack(player):
     choice = input(f"{players.getPlayer(player)[0]}, Rood (1) of Zwart (2): ")
     card = cards.getCard()
@@ -34,6 +34,7 @@ def redBlack(player):
             return True, card
     return False, card
 
+#Second round of round 1, higher or lower then the previous card
 def highLow(player):
     currentcard = players.getCardArray(player)[0]
     card = cards.getCard()
@@ -47,10 +48,33 @@ def highLow(player):
         if card[1] < currentcard[1]:
             return False, card
     return True, card
-    
-    
+
+#Decides if card is inner or outer    
+def innerOuter(player):
+    currentcards = players.getCardArray(player)
+    tempcurrent1 = currentcards[0][1]
+    tempcurrent2 = currentcards[1][1]
+    if tempcurrent1 > tempcurrent2:
+        lowest = tempcurrent2
+        highest = tempcurrent1
+    else:
+        lowest = tempcurrent1
+        highest = tempcurrent2
+    card = cards.getCard()
+    playerName = players.getPlayer(player)[0]   
+    players.printCurrendPlayerCards(player)
+    choice = int(input("Binnen (1) of buiten (2)"))
+
+    if choice == 1:
+        if lowest < card[1] and highest > card[1]:
+            return False, card
+    if choice == 2:
+        if lowest > card[1] and highest < card[1]:
+            return False, card
+    return True, card
 
 
+#Add cards, add drinks, to player object, logic works for round 1
 def adjustScoreRound1(player, card, drinkBool):
     if drinkBool == True:
         players.addDrink(1, player)
@@ -62,6 +86,7 @@ def adjustScoreRound1(player, card, drinkBool):
     players.printCurrendPlayerCards(player)
     print('--------------------------------\n')
 
+#Calls all the logic to play  out round 1 (colour, high/low, in/outer, has (not) suit)
 def roundOne():
     for i in range(players.getPlayerCount()):
         hasToDrink, card = redBlack(i)
@@ -70,6 +95,14 @@ def roundOne():
     for i in range(players.getPlayerCount()):
         hasToDrink, card = highLow(i)
         adjustScoreRound1(i, card, hasToDrink)
-        
-startGame()
+
+    for i in range(players.getPlayerCount()):
+        hasToDrink, card = innerOuter(i)
+        adjustScoreRound1(i, card, hasToDrink)
+
+
+players.addPlayer("Henk")
+players.addPlayer("Klaas")
+players.addPlayer("Jan")
+
 roundOne()
